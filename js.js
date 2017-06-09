8aa33ea81e008105c7b2da5092408a884a7144d7
galynag <g.alenicheva@gmail.com>
2017-06-09 16:54:18 +0300

    v 0.2 style was added

--- js.js
+++ js.js
@@ -1,16 +1,17 @@
+//массив основных блюд
 var gamburgers = {
-    'маленький' : {
+    'Гамбургер маленький' : {
                 'cost' : 17,
                 'ccal' : 250,
                 'id' : 123
                 },
-    'большой' : {
+    'Гамбургер большой' : {
                 'cost' : 25,
                 'ccal' : 340,
                 'id' : 124
             }
 };
-
+//массив дополнений к основным блюдам
 var optional = {
     'сыр' : {
         'cost' : 4,
@@ -43,7 +44,7 @@ var optional = {
         'id' : 'op4'
     }
 };
-
+//массив для вывода информации в чек и localStOrders для сумм, которые мы потом добавляем в localStorage
 var order = {
     'sum' : 0,
     'ccal' : 0,
@@ -51,8 +52,7 @@ var order = {
     'out' : '',
     'localStOrders':[]
 };
-
-
+//функция отрисовки меню на странице
 function renderItems() {
     for(var key in gamburgers) {
         $('<input/>', {
@@ -63,11 +63,10 @@ function renderItems() {
             'data-ccal' : gamburgers[key].ccal,
             'data-cost': gamburgers[key].cost,
             type: 'radio'
-
         }).appendTo('#gamb');
         $('<label/>', {
             for: gamburgers[key].id,
-            text: 'Гамбургер '+key+', '+ gamburgers[key].cost + 'грн., '+gamburgers[key].ccal+'ккал'
+            text: key+', '+ gamburgers[key].cost + 'грн., '+gamburgers[key].ccal+'ккал'
         }).appendTo('#gamb');
     }
     for (var key in optional) {
@@ -78,7 +77,6 @@ function renderItems() {
             type: 'checkbox',
             'data-ccal' : optional[key].ccal,
             'data-cost': optional[key].cost,
-
         }).appendTo('#optional');
         $('<label/>', {
             for: optional[key].id,
@@ -86,31 +84,38 @@ function renderItems() {
         }).appendTo('#optional');
     }
     $('<p/>', {
-        text: '* нужно обязательно выбрать хотя бы одно дополнение, отмеченное *'
+        class: 'helper',
+        text: 'Нужно обязательно выбрать хотя бы одно дополнение, отмеченное *'
     }).appendTo('#optional');
+
 }
 renderItems();
-
+//функция отображения списка заказанной еды+сумма+калории
 function addFood() {
-
+    delOrder();
+    if ($('input:checked').length > 0) {
+        console.log($('input:checked'));
     $('input:checked').map( function (index,el) {
         order.sum = order.sum + Number($(el).attr('data-cost'));
         order.ccal = order.ccal + Number($(el).attr('data-ccal'));
         order.items.push($(el).attr('value'));} );
 
     console.log(order);
-    order.out+='<p><b>Состав заказа:</b></p><ol>';
+    order.out+='<br><p><b>Состав заказа:</b></p><ol>';
     for (var i=0; i<order.items.length; i++) {
         order.out+='<li>'+order.items[i]+'</li>';
     }
-    order.out+='</ol>';
+    order.out+='</ol><br>';
     order.out+='<p><b>Cумма заказа: </b>'+order.sum+'</p>';
     order.out+='<p><b>Всего калорий: </b>'+order.ccal+'</p>';
     $('#out').append(order.out);
     $('#gamb').empty();
     $('#optional').empty();
-    renderItems();
+    renderItems();} else {
+        $('#out').append('<p class="mistake">Вы не выбрали ни одного блюда</p>');
+    }
 };
+//обнуление данных по чеку
 function delOrder() {
     order.sum = 0;
     order.ccal = 0;
@@ -119,7 +124,7 @@ function delOrder() {
     $('#out').empty();
     $('input').attr('checked',false);
 };
-
+//функция добавления данных в localStorage
 function toLocalStorage() {
     order.localStOrders.push(order.sum);
     var serialObj = JSON.stringify(order.localStOrders);
@@ -131,6 +136,7 @@ function toLocalStorage() {
 //         }
 //     }
 }
+//функция вычисления средней суммы чеков по данным из localStorage
 function middleOrder() {
     $('#mid').empty();
     var returnObj = JSON.parse(localStorage.getItem('order'));
@@ -147,7 +153,8 @@ function middleOrder() {
 
 $('#addFood').on('click',addFood);
 
-// $('#delOrder').on('click',delOrder);
+
+$('#delOrder').on('click',delOrder);
 $('#pay').on('click',toLocalStorage);
 
 $('#middle-order').on('click',middleOrder);
